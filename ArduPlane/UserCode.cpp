1bf76849b6e6a7ed01610f87887282903240c602
Thomas Eliot <thomaskeliot@gmail.com>
2024-12-14 22:42:21 -0800

    battery heater control code

--- ArduPlane/UserCode.cpp
+++ ArduPlane/UserCode.cpp
@@ -1,4 +1,9 @@
 #include "Plane.h"
+#include "AP_BattMonitor/AP_BattMonitor.h"
+#include "AP_HAL/AP_HAL.h"
+
+#define HEAT_BATTERY_BELOW_TEMP_C 28.0f
+#define RELAY_INDEX 0
 
 #ifdef USERHOOK_INIT
 void Plane::userhook_init()
@@ -32,8 +37,27 @@ void Plane::userhook_MediumLoop()
 #ifdef USERHOOK_SLOWLOOP
 void Plane::userhook_SlowLoop()
 {
-    GCS_SEND_TEXT(MAV_SEVERITY_INFO, "slow loop");
-    // put your 3.3Hz code here
+
+    float battery_temperature_c = -300.0f;
+    bool result = battery.get_temperature(battery_temperature_c);
+
+    // OH crap, seems like get_temperature still returns true when I disconnect the sensor. 
+    if (!result || battery_temperature_c < -100.0f){
+        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Failed to get batt temp (%f), disabling heater", battery_temperature_c);
+        relay.off(RELAY_INDEX);
+    }
+    else if (battery_temperature_c < HEAT_BATTERY_BELOW_TEMP_C && !relay.get(RELAY_INDEX))
+    {
+        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "batt temp is low at (%f°C), heat on", battery_temperature_c);
+        // Turning the relay on turns on the heater.
+        relay.on(RELAY_INDEX);
+    }
+    else if (battery_temperature_c > HEAT_BATTERY_BELOW_TEMP_C && relay.get(RELAY_INDEX))
+    {
+        GCS_SEND_TEXT(MAV_SEVERITY_INFO, "batt temp is high at (%f°C), heat off", battery_temperature_c);
+        relay.off(RELAY_INDEX);
+    }
+
 }
 #endif
 
